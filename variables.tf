29e090da938bd96fdf4792aa485fc74c35511302
Sergio Artero <sergio.staff@gmail.com>
2024-09-13 00:38:04 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "instance_type_ec2" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "db_instance_type" {
-    type = string
-    default = "db.t3.micro"
+  type    = string
+  default = "db.t3.micro"
 }
 
 variable "db_engine" {
-    type = string
-    default = "postgres"
+  type    = string
+  default = "postgres"
 }
 
 variable "db_version" {
-    type = string
-    default = "14.9"
+  type    = string
+  default = "14.9"
 }
 
 variable "db_description" {
-    type = string
-    default = "quick-serve-db"
+  type    = string
+  default = "quick-serve-db"
 }
 
 variable "db_database_name" {
-    type = string
-    default = "quickserve"
+  type    = string
+  default = "quickserve"
 }
 
 variable "db_storage" {
-    type = number
-    default = 20
+  type    = number
+  default = 20
 }
 
 variable "db_user" {
-    type = string
-    default = "root"
+  type    = string
+  default = "root"
 }
 
 variable "db_password" {
-    type = string
-    default = "RootFiap123"
+  type    = string
+  default = "RootFiap123"
 }
